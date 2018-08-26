540060db4975fe98c8a54d270f5911dbff96dde4
mazc <mzc_2015@163.com>
2018-08-26 21:25:53 +0800

    add inherit in syntax.py

--- syntax.py
+++ syntax.py
@@ -239,4 +239,37 @@ class Student(object):
         print('%s: %s' % (self.__name, self.__score))
 
 # 继承和多态
+# 在OOP程序设计中，当我们定义一个class的时候，可以从某个现有的class继承，新的class称为子类（Subclass），而被继承的class称为基类、父类或超类（Base class、Super class）。
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+class Dog(Animal):
+    pass
+class Cat(Animal):
+    pass
+dog = Dog()
+dog.run()
+cat = Cat()
+cat.run()
+# 当子类和父类都存在相同的run()方法时，我们说，子类的run()覆盖了父类的run()，在代码运行的时候，总是会调用子类的run()。这样，我们就获得了继承的另一个好处：多态。
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+# 多态的好处就是，当我们需要传入Dog、Cat、Tortoise……时，我们只需要接收Animal类型就可以了，因为Dog、Cat、Tortoise……都是Animal类型，然后，按照Animal类型进行操作即可。由于Animal类型有run()方法，因此，传入的任意类型，只要是Animal类或者子类，就会自动调用实际类型的run()方法，这就是多态的意思：
+def run_twice(animal):
+    animal.run()
+    animal.run()
+run_twice(Dog())
+run_twice(Cat())
+# 静态语言 vs 动态语言
+# 对于静态语言（例如Java）来说，如果需要传入Animal类型，则传入的对象必须是Animal类型或者它的子类，否则，将无法调用run()方法。
+# 对于Python这样的动态语言来说，则不一定需要传入Animal类型。我们只需要保证传入的对象有一个run()方法就可以了：
+class Timer(object):
+    def run(self):
+        print('Start...')
+# 这就是动态语言的“鸭子类型”，它并不要求严格的继承体系，一个对象只要“看起来像鸭子，走起路来像鸭子”，那它就可以被看做是鸭子。
+
 
