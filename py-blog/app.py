1d635d40e5698525ce21109b2a68d8448ff7d86e
mazc <mzc_2015@163.com>
2018-12-09 09:20:58 +0800

    add py blog

---
+++ py-blog/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @File    : app.py
+# @Time    : 2018/12/06
+# @Author  : mazc (mzc_2015@163.com)
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',headers={'content-type':'text/html'})
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
